0ef61860360e187365a7fd80d4edc4b801e043ad
henry cho <henrycho@henrys-MacBook-Pro.local>
2021-12-02 17:31:12 +0900

    [UPDATE] 폴더구조 정리

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import Header from './components/Header/Header';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
-import Login from './components/Login';
 import MyPage from './pages/MyPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 import { useSelector } from 'react-redux';
 import PrivateRoute from './components/PrivateRoute';
@@ -14,15 +14,15 @@ function App() {
 
   return (
     <div className="App">
-      <Header authenticated/>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Main />}/>
-        <Route exact path="/login" element={<Login />}/>
+        <Route exact path="/login" element={<LoginPage />}/>
         <Route 
           path="/myPage"
           element={
             <PrivateRoute authenticated>
-              <MyPage />
+              <MyPage authenticated/>
             </ PrivateRoute>
           }
         />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,65 +1,13 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { auth_actions } from '../../_actions/auth_action';
-
-import { LoginButton, LogOutButton } from '../Button.styles';
 
 import Nav from './Nav/Nav';
-
 import './Header.css';
 
 function Header() {
-
-  const authenticated = useSelector(store => store.auth.authenticated)
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    const accessToken = localStorage.getItem("ACCESS_TOEN")
-    console.log(authenticated)
-    // return accessToken ? handleLogin : handleLogout
-  })
-
-  const handleLogin = () => {
-    dispatch(auth_actions.login())
-  }
-
-  const handleLogout = () => {
-    dispatch(auth_actions.logout())
-  }
-
   return (
     <header>
-      <div className="wrapper">
-        <Nav authenticated/>
-        {/* <nav>
-          <div>
-            Header
-          </div>
-          <div>
-            {
-              authenticated?
-              
-              <div>
-                <ul>
-                  <li><LogOutButton onClick={handleLogout}>로그아웃 테스트</LogOutButton></li>
-                  <li>견적요청</li>
-                  <li>마이 페이지</li>
-                  <li>로그아웃</li>
-                </ul>
-              </div>
-
-              :
-
-              <div>
-                <ul>
-                  <li><LoginButton onClick={handleLogin}>로그인 테스트</LoginButton></li>
-                  <li>로그인</li>
-                </ul>
-              </div>
-              
-            }
-          </div>
-        </nav> */}
+      <div>
+        <Nav />
       </div>
     </header>
   )
