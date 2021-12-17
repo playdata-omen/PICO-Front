145708c4c3aa8201173dc503a070b59af0677431
henry cho <henrycho@henrys-MacBook-Pro.local>
2021-12-18 02:46:37 +0900

    [NEW] redux-persist

--- src/config/store.js
+++ src/config/store.js
@@ -2,6 +2,8 @@ import { applyMiddleware, createStore } from 'redux';
 import rootReducer from '../_reducers';
 
 import ReduxThunk from 'redux-thunk';
+import { persistStore } from 'redux-persist' 
+import { PersistGate } from 'redux-persist/integration/react' 
 
 import { composeWithDevTools } from 'redux-devtools-extension';
 
@@ -9,3 +11,5 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 export const store = createStore(
   rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk)) 
 );
+
+export const persistor = persistStore(store)
