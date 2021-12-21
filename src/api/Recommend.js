b7da51a2ea095f754abc92ee2c99f3094a9a164b
kingdomunder <king4525@gmail.com>
2021-12-22 05:23:26 +0900

    [UPDATE] 프론트 imageSearchPage 연동

--- src/api/Recommend.js
+++ src/api/Recommend.js
@@ -1,31 +1,30 @@
 import API from "./API"
+import axios from 'axios'
 
-export const getRecommendReqList = async uploadedImg => {
-  const data = await API.get('getRecommendList', uploadedImg)
+export const getRecommendList = async image => {
+  const data = await API.post('getRecommendList', image)
   .then(res => {
     return res.data
   })
   .catch(err => {
-    return [
-      {"rank" : 1, "photoIdx" : 1, "workIdx" : 2, "storedFilePath" : "URL1"},
-      {"rank" : 2, "photoIdx" : 2, "workIdx" : 3, "storedFilePath" : "URL2"},
-      {"rank" : 3, "photoIdx" : 3, "workIdx" : 4, "storedFilePath" : "URL3"},
-      {"rank" : 4, "photoIdx" : 4, "workIdx" : 2, "storedFilePath" : "URL4"},
-    ]
+    return []
   })
   return data
 }
 
-export const getRecommendDetail = async workIdx => {
-  const data = await API.get('estimateDetail', workIdx)
-  .then(res => {
-    return res.data
-  }).catch(err => {
-    return (
-      // 추천받은 사진을 눌렀을 때, 작가정보를 받아올 지 작품 정보를 받아올 지
-      // 어디로 리다이렉트 하느냐에 따라 
-      "정보"
-    )
+export const getRecommendListFromFlask = async image => {
+  const FlaskAPI = axios.create({
+    baseURL: "http://localhost:5000/"
+    // baseURL: SERVER_URL,
   })
+
+  let formData = new FormData();
+  formData.append("userFile", image);
+  const data = await FlaskAPI.post('searchImage', formData)
+    .then(res => {
+      return res.data
+    }).catch(err => {
+    })
+
   return data
 }
