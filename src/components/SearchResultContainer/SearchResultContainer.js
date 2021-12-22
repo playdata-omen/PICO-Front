2aa96f13cbbe1081c99793de094250d44666d1c9
Henry <henrynoowah@gmail.com>
2021-12-22 09:15:36 +0900

    [FIX] ProfilePage Routing params naming

--- src/components/SearchResultContainer/SearchResultContainer.js
+++ src/components/SearchResultContainer/SearchResultContainer.js
@@ -11,7 +11,7 @@ function SearchResultContainer({ type, search }) {
   const [loading, setLoading] = useState(true)
   const [result, setResult] = useState()
 
-  const PhotographerPage = userIdx => navigate(`/profile/${userIdx}`)
+  const photographerPage = userIdx => navigate(`/profile/${userIdx}`)
 
   useEffect( async() => {
     console.log(type)
@@ -35,7 +35,7 @@ function SearchResultContainer({ type, search }) {
       <div className={styles.container}>
         {
           result.map(res =>
-            <div onClick={() => PhotographerPage(res.user.userIdx)}>
+            <div onClick={() => photographerPage(res.user.userIdx)}>
               <MyInfoCard user={res.user} />
             </div>
           )
