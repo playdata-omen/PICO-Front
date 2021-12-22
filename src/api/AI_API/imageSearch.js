4ba0c6cc0b2764e461e33652253687edaf5b83c9
kingdomunder <king4525@gmail.com>
2021-12-22 10:44:26 +0900

    [UPDATE] API 재설정 / 파일명 변경

---
+++ src/api/AI_API/imageSearch.js
@@ -0,0 +1,13 @@
+import AI_API from "./AI_API"
+
+export const getRecommendList = async image => {
+  let formData = new FormData();
+  formData.append("userFile", image);
+  const data = await AI_API.post('searchImage', formData)
+    .then(res => {
+      return res.data
+    }).catch(err => {
+    })
+
+  return data
+}
